1f20ac096536a3bfd69fa4684d6f0fd7034630be
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-06-13 19:37:04 -0700

    Lab 1 no main

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+
+class Footer extends React.Component {
+    render() {
+        return <footer>John Rayome, 2022</footer>;
+    }
+}
+
+export default Footer;
