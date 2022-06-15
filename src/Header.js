f9b4ad5aec5995c7f103e331da01727f46a6663d
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-06-14 18:52:29 -0700

    Lab 02 Complete

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
 class Header extends React.Component {
     render() {
         return (
         <header>
-            <h1>Gallery of Horns</h1>
+            <h1>Gallery of Horns
+            </h1>
         </header>
         );  
     }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,40 @@
 import React from "react";
+import data from "./data.json";
+import './HornedBeast.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votes: 0
+        };
+    };
+
+handleVotes = () => {
+    this.setState({
+        votes: this.state.votes + 1
+    })
+}
+
+
+
 render () {
     return (
-        <article>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
-            <p>{this.props.description}</p>
-        </article>
-    )
-}
+    <article>
+        <Card bg="dark" text="light" style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>{this.props.description}</Card.Text>
+    <Card.Text>💖{this.state.votes} times liked.</Card.Text>
+    <Button variant="primary" onClick={this.handleVotes}> Pick Me!</Button>
+  <Card.Img variant="bottom" src={this.props.image_url} onClick={this.handleVotes}/>
+  </Card.Body>
+</Card>
+</article>
+    );
+};
 
 
 
